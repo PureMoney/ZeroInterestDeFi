bddb09a7e6f82fb1f373aea9bf2db4aabc94897b
Carlos Tapang <carlos.tapang@rockstable.io>
2021-05-18 00:11:12 -0700

    dapp-scaffold for working with devnet

---
+++ build/precache-manifest.9b981a9080f3c95e4e40e465e0dd0bc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888200ee0e12ed79db8d7563859915e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97b45af67f8e283ae85b",
+    "url": "./static/css/main.b087b3cf.chunk.css"
+  },
+  {
+    "revision": "706ed28ba328d55769e7",
+    "url": "./static/js/2.889a5a2e.chunk.js"
+  },
+  {
+    "revision": "ca228484939fe4d1fc04f68ed86646c0",
+    "url": "./static/js/2.889a5a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b45af67f8e283ae85b",
+    "url": "./static/js/main.6c8c667e.chunk.js"
+  },
+  {
+    "revision": "da503e7975902b161ea8",
+    "url": "./static/js/runtime-main.7d63d1b5.js"
+  },
+  {
+    "revision": "a4e80c1e977416ec45d808a3268892b7",
+    "url": "./static/media/builton.a4e80c1e.svg"
+  }
+]);
