027887ffdb3efd695bda5e2045495566ad7322a6
Carlos Tapang <carlos.tapang@rockstable.io>
2021-05-30 18:42:24 -0700

    This version works with Pyth oracle for getting price of SOL

--- anchor/oracles/pyth/programs/pyth/src/lib.rs
+++ anchor/oracles/pyth/programs/pyth/src/lib.rs
@@ -1,39 +1,87 @@
 use anchor_lang::prelude::*;
+use pyth_client::{
+    CorpAction, 
+    PriceStatus, 
+    PriceType,
+};
 mod pc;
-use pc::Price;
+use pc::*;
 
 #[program]
 pub mod pyth {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, price: i64, expo: i32, conf: u64) -> ProgramResult {
-        let oracle = &ctx.accounts.price;
-
-        let mut price_oracle = Price::load(&oracle).unwrap();
-
-        price_oracle.agg.price = price;
-        price_oracle.agg.conf = conf;
-        price_oracle.expo = expo;
-        price_oracle.ptype = pc::PriceType::Price;
+    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
     }
 
-    pub fn set_price(ctx: Context<SetPrice>, price: i64) -> ProgramResult {
-        let oracle = &ctx.accounts.price;
-        let mut price_oracle = Price::load(&oracle).unwrap();
-        price_oracle.agg.price = price as i64;
+    pub fn get_price(ctx: Context<GetPrice>) -> ProgramResult {
+        let pyth_product_info = &mut ctx.accounts.pyth_product_info;
+        let pyth_price_info = &mut ctx.accounts.pyth_price_info;
+    
+        let pyth_product_data = &pyth_product_info.try_borrow_data()?;
+        let pyth_product = pyth_client::cast::<pyth_client::Product>(pyth_product_data);
+    
+        if pyth_product.magic != pyth_client::MAGIC {
+            msg!("Pyth product account provided is not a valid Pyth account");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+        if pyth_product.atype != pyth_client::AccountType::Product as u32 {
+            msg!("Pyth product account provided is not a valid Pyth product account");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+        if pyth_product.ver != pyth_client::VERSION_1 {
+            msg!("Pyth product account provided has a different version than the Pyth client");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+        if !pyth_product.px_acc.is_valid() {
+            msg!("Pyth product price account is invalid");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+    
+        let pyth_price_pubkey = Pubkey::new(&pyth_product.px_acc.val);
+        if &pyth_price_pubkey != pyth_price_info.key {
+            msg!("Pyth product price account does not match the Pyth price provided");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+    
+        let pyth_price_data = &pyth_price_info.try_borrow_data()?;
+        let pyth_price = pyth_client::cast::<pyth_client::Price>(pyth_price_data);
+    
+        msg!("  price_account .. {:?}", pyth_price_info.key);
+        msg!("    price_type ... {}", get_price_type(&pyth_price.ptype));
+        msg!("    exponent ..... {}", pyth_price.expo);
+        msg!("    status ....... {}", get_status(&pyth_price.agg.status));
+        msg!(
+            "    corp_act ..... {}",
+            get_corp_act(&pyth_price.agg.corp_act)
+        );
+        msg!("    price ........ {}", pyth_price.agg.price);
+        msg!("    conf ......... {}", pyth_price.agg.conf);
+        msg!("    valid_slot ... {}", pyth_price.valid_slot);
+        msg!("    publish_slot . {}", pyth_price.agg.pub_slot);
+    
         Ok(())
     }
-}
-
-#[derive(Accounts)]
-pub struct SetPrice<'info> {
-    #[account(mut)]
-    pub price: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(mut)]
-    pub price: AccountInfo<'info>,
+    #[account(init)]
+    pub my_account: ProgramAccount<'info, PythData>, // acct owned by program, PythData unused
+    pub rent: Sysvar<'info, Rent>,
 }
+
+#[derive(Accounts)]
+pub struct GetPrice<'info> {
+    #[account(mut)]
+    pub my_account: ProgramAccount<'info, PythData>, // acct owned by program, PythData unused
+    pub pyth_product_info: AccountInfo<'info>,
+    pub pyth_price_info: AccountInfo<'info>,
+}
+
+#[account]
+pub struct PythData {
+    pub data: u64,
+}
+
---
+++ anchor/oracles/pyth/tests/pyth.js
@@ -0,0 +1,61 @@
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+
+describe("Pyth Tests on Node", () => {
+  // Use a local provider.
+  const provider = anchor.Provider.env(); //.local();
+
+  // console.log('provider = ', provider);
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(provider);
+
+  // The signing Account.
+  const myAccount = anchor.web3.Keypair.generate(); // .Wallet.local().payer;
+
+  it("Initialize Pyth", async () => {
+    // The program owning the account to create.
+    const program = anchor.workspace.Pyth;
+
+    // Atomically create the new account and initialize it with the program.
+    await program.rpc.initialize({
+      accounts: {
+        myAccount: myAccount.publicKey,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [myAccount],
+      instructions: [
+        anchor.web3.SystemProgram.createAccount({
+          fromPubkey: provider.wallet.publicKey,
+          newAccountPubkey: myAccount.publicKey,
+          space: 8 + 8, // Add 8 for the account discriminator.
+          lamports: await provider.connection.getMinimumBalanceForRentExemption(
+            8 + 8
+          ),
+          programId: program.programId,
+        }),
+      ],
+    });
+
+    assert.ok(true);
+  });
+
+  it("Get SOL Price", async() => {
+    // The program owning the account to create.
+    const program = anchor.workspace.Pyth;
+
+    const pythProdKey = new anchor.web3.PublicKey("8yrQMUyJRnCJ72NWwMiPV9dNGw465Z8bKUvnUC8P5L6F");
+    const pythSOLPriceProgKey = new anchor.web3.PublicKey("BdgHsXrH1mXqhdosXavYxZgX6bGqTdj5mh2sxDhF8bJy");
+
+    // Execute the RPC.
+    // #region code-separated
+    await program.rpc.getPrice({
+      accounts: {
+        myAccount: myAccount.publicKey,
+        pythProductInfo: pythProdKey,
+        pythPriceInfo: pythSOLPriceProgKey
+      }
+    });
+    // #endregion code-separated
+  });
+});
